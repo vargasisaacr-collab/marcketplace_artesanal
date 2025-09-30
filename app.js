5c5c854584d631d67635f9527c1528721b120c52
vargasisaacr-collab <vargas.isaacr@gmail.com>
2025-09-29 22:30:00 -0500

    app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const productos = [
-  { id: 1, nombre: "Sombrero de Paja Toquilla", precio: 25, img: "img/sombrero.jpg" },
-  { id: 2, nombre: "Collar Artesanal", precio: 15, img: "img/collar.jpg" },
-  { id: 3, nombre: "Bolso Tejido", precio: 35, img: "img/bolso.jpg" },
-  { id: 4, nombre: "Cerámica Decorativa", precio: 40, img: "img/ceramica.jpg" }
+  { id: 1, nombre: "Sombrero de Paja Toquilla", precio: 25, img: "https://images.unsplash.com/photo-1617196034794-8d5b2c951f3b" },
+  { id: 2, nombre: "Collar Artesanal", precio: 15, img: "https://images.unsplash.com/photo-1587049352847-0d7e33539e88" },
+  { id: 3, nombre: "Bolso Tejido", precio: 35, img: "https://images.unsplash.com/photo-1602810318383-e71dc0d6a5d5" },
+  { id: 4, nombre: "Cerámica Decorativa", precio: 40, img: "https://images.unsplash.com/photo-1617191519605-fc0f7e3b6a27" }
 ];
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
       const div = document.createElement("div");
       div.classList.add("producto");
       div.innerHTML = `
-        <img src="${prod.img}" alt="${prod.nombre}">
+        <img src="${prod.img}" alt="${prod.nombre}" width="200">
         <h3>${prod.nombre}</h3>
         <p>Precio: $${prod.precio}</p>
         <button>Comprar</button>
@@ -21,3 +21,4 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+
